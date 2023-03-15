cffc1b807eecbcf1b4798fb8d9d37bf33d3820c5
emanoelbrenneisen <emanoelbrenneisen@gmail.com>
2023-03-15 16:45:04 -0300

    Texto adicionado

--- src/screens/TelaIntro2.jsx
+++ src/screens/TelaIntro2.jsx
@@ -7,7 +7,7 @@ export const TelaIntrodução2 = ({ navigation }) => {
         <View style={intro.fundo}>
 
             <ImageBackground source={require('../imagens/salaa.png')} style={intro.backgroundImage}>
-                <Text onPress={() => navigation.navigate("Inicio")}>Pular</Text>
+                <Text style={intro.pular} onPress={() => navigation.navigate("Inicio")}>Pular</Text>
 
                 <View
                     style={intro.setas}
@@ -17,6 +17,7 @@ export const TelaIntrodução2 = ({ navigation }) => {
                     <Icon.Button style={intro.botico} name="right" size={25} color="#6f7f76" backgroundColor="none" onPress={() => navigation.navigate("Introducao3")} >
                     </Icon.Button>
                 </View>
+                <Text style={intro.TextIntro}>Nisi lacus sed viverra tellus in hac habitasse platea dictumst. Fringilla phasellus faucibus scelerisque eleifend donec.</Text>
             </ImageBackground>
 
         </View>
