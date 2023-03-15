b6e299c5e4361c4a717daa3e1a28678d45ed0740
cintiavz <cintia.vizentaener@alunos.sc.senac.br>
2023-03-15 17:17:21 -0300

    terminando

--- src/screens/TelaCadastroCartao.jsx
+++ src/screens/TelaCadastroCartao.jsx
@@ -26,28 +26,45 @@ export const TelaCadastroCartao = ({ navigation }) => {
         />
 </View>
 <View style={styles.lateral}>
-  <View style={styles.container}>
+  <View>
       <Picker
         selectedValue={selectedValue}
-        style={{ height: 50, width: 145}}
+        style={{ height: 50, width: 140}}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
       >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
+        <Picker.Item label="01" />
+        <Picker.Item label="02" />
+        <Picker.Item label="03" />
+        <Picker.Item label="04" />
+        <Picker.Item label="05" />
+        <Picker.Item label="06" />
+        <Picker.Item label="07" />
+        <Picker.Item label="08" />
+        <Picker.Item label="09" />
+        <Picker.Item label="10" />
+        <Picker.Item label="11" />
+        <Picker.Item label="12" />
       </Picker>
     </View>
-    <View style={styles.container}>
+    <View>
       <Picker
         selectedValue={selectedValue}
-        style={{ height: 50, width: 145}}
+        style={{ height: 50, width: 140}}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
+
+          <Picker.Item label="2020" />
+          <Picker.Item label="2021" />
+          <Picker.Item label="2023"/>
+          <Picker.Item label="2024" />
+          <Picker.Item label="2025" />
+          <Picker.Item label="2026" />
+          <Picker.Item label="2027" />
+          <Picker.Item label="2028" />
+          <Picker.Item label="2029" />
       </Picker>
-     
+
+     </View>
     </View>
   </View>
-</View>
-  
   );
 };
